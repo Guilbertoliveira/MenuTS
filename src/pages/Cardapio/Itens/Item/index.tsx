354ec41ea5d7ae4c379bb752aa5247b68b5b2e0d
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-08-23 22:55:31 -0300

    chore: dynamic photo insertion and background color

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,10 +1,13 @@
 import styles from './Item.module.scss'
 import cardapio from '../itens.json'
+import classNames from 'classnames';
 
 type Props = typeof cardapio[0];
 
 export default function Item(props: Props) {
     const { title, description, photo, category, price, size, serving } = props
+
+
     return (
         <div className={styles.item}>
             <div className={styles.item__imagem}>
@@ -16,10 +19,14 @@ export default function Item(props: Props) {
                     <p>{description}</p>
                 </div>
                 <div className={styles.item__tags}>
-                    <span className={styles.item__tipo}>{category.label}</span>
+                    <span className={classNames({
+                        [styles.item__tipo]: true,
+                        [styles[`item__tipo__${category.label.toLocaleLowerCase()}`]]: true
+                    })}>{category.label}</span>
                     <span className={styles.item__porcao}>{size}g</span>
                     <span className={styles.item__qtdpessoas}>Serve {serving} pessoa{serving > 1 ? 's' : ''}</span>
                     <span className={styles.item__valor}>R${price.toFixed(2)}</span>
+                    {/* <span className={styles[`item__tipo__${category.label.toLowerCase()}`]}>teste</span> */}
                 </div>
             </div>
         </div>
